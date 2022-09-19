2cf5f07c5c55043d33f5b323eb58700d18293f59
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2022-09-19 15:02:09 +0300

    Add files via upload

---
+++ js13 - ornek/script.js
@@ -0,0 +1,19 @@
+// //Challenge
+// Bu Codepen'in JS bölümünde şu koşulu kontrol eden bir if ifadesi yazalım: firstWord değişkeni secondWord değişkenine eşit mi?
+
+// Cevap evet ise message değişkenini "aynı" dizesine ayarlayın.
+// Hayır ise firstWord'ün secondWord'den daha uzun olup olmadığını kontrol edin. (daha uzun - metnin harf uzunluğu anlamına gelir)
+// Evet ise message değişkenini "uzun" dizesine ayarlayın.
+// Aksi takdirde (else) message değişkenini "kısa" olarak ayarlayın
+
+
+const firstWord = "peony";
+const secondWord = "daffodil";
+let message = "kisa";
+
+if(firstWord === secondWord) {
+    console.log(`Ayni`)
+} else if(firstWord.length > secondWord.length){
+    console.log(`uzun`)
+} else(console.log(`${message}`))
+
